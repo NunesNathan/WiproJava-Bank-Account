fcb5bf2cc5a50c0db2d6a2807d7f983ffa21ac20
NunesNathan <nunesxd7@hotmail.com>
2022-09-09 16:09:45 -0300

    feat: acc number formatter works

--- src/main/java/com/betrybe/acc/java/bankaccount/AccountNumberFormatter.java
+++ src/main/java/com/betrybe/acc/java/bankaccount/AccountNumberFormatter.java
@@ -1,10 +1,13 @@
 package com.betrybe.acc.java.bankaccount;
 
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * AccountNumberFormatter.
  */
 public class AccountNumberFormatter {
+  final static int lengthNecessary = 6;
 
   /**
    * Formata o número da conta.
@@ -12,7 +15,18 @@ public class AccountNumberFormatter {
    * @param literalAccountNumber número da conta
    */
   public String formatAccountNumber(int literalAccountNumber) {
+    String accountNumber = String.valueOf(literalAccountNumber);
+    int accountNumberLength = accountNumber.length();
 
+    if (accountNumberLength < lengthNecessary) {
+      return StringUtils.leftPad(accountNumber, lengthNecessary, '0');
+    }
+
+    if (accountNumberLength > lengthNecessary) {
+      return StringUtils.right(accountNumber, lengthNecessary);
+    }
+
+    return accountNumber;
   }
 
 }
