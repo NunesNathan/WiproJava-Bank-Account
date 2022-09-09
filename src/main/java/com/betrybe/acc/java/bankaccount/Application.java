ab508cc55ea54f11c1a18829e2a129503dbdbe39
NunesNathan <nunesxd7@hotmail.com>
2022-09-09 16:10:53 -0300

    feat: app client workflow

--- src/main/java/com/betrybe/acc/java/bankaccount/Application.java
+++ src/main/java/com/betrybe/acc/java/bankaccount/Application.java
@@ -1,5 +1,7 @@
 package com.betrybe.acc.java.bankaccount;
 
+import java.util.Scanner;
+
 /**
  * A class.
  */
@@ -9,7 +11,21 @@ public class Application {
    * A simple get method.
    */
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    AccountNumberFormatter anf = new AccountNumberFormatter();
+    int account = 0;
 
+    System.out.println("Informe o número da conta:");
+
+    try {
+      account = scan.nextInt();
+    } catch (NumberFormatException e) {
+      System.out.println("Número da conta inválido!");
+    } finally {
+      scan.close();
+    }
+
+    String formattedAccountNumber = anf.formatAccountNumber(account);
+    System.out.printf("Numero da conta: " + formattedAccountNumber);
   }
-
 }
